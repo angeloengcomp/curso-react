e7dc890ce9bccad8db1f740662a09aebe6e497c1
Angelo Ricardo <angeloengcomp@gmail.com>
2021-04-16 18:20:18 -0300

    adição do parseint e o destructuring

--- fundamentos/src/components/basics/NumAleatorio.jsx
+++ fundamentos/src/components/basics/NumAleatorio.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-export default (props) => {
+export default props => {
+    //processo de destructuring
+    let min = props.min
+    let max = props.max
 
     //logica para gerar numero aleatorio entre 0,1 >> Math.random
     //Logica para gerar numero aleatorio entre dois valores >> Math.random()*(props.max-props.min)+props.min
-    const valor = Math.random()*(props.max-props.min)+props.min
+    const valor = parseInt(Math.random()*(max-min)+min)
 
     return (
 
         <div>    
-            <h1>Número Aleatório {valor}</h1>
+            <h1>Número Aleatório entre {min} e {max}: {valor}</h1>
         </div>   
 
     )

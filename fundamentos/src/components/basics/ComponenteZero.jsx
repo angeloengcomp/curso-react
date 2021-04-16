154a70bc3a00676ca1475678c714dd49ccf47e36
Angelo Ricardo <angeloengcomp@gmail.com>
2021-04-16 02:55:21 -0300

    git bash

---
+++ fundamentos/src/components/basics/ComponenteZero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ComponenteZero(){
+    return (
+        <>
+        <div>
+            <h1>
+                Testando Componente Zero
+            </h1>
+        </div>
+        </>
+    )
+}

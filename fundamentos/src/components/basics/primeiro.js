481e310a7d72641be6e4c21c1ca69b95db79f563
Angelo Ricardo <angeloengcomp@gmail.com>
2021-04-16 01:37:01 -0300

    Update primeiro.js

--- fundamentos/src/components/basics/primeiro.js
+++ fundamentos/src/components/basics/primeiro.js
@@ -1,3 +1,15 @@
+import React from 'react'
+
+let msg = 'Welcome!'
 export default function primeiro (){
-    return 'primeiro componente'
+    return (
+        <div>
+            <h2>My First Component</h2>
+            <p>{msg}</p>
+
+
+        </div>
+    )
+
+
 }

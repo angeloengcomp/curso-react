070ca4ed8cb1bfcb6c6207d2a9ef4e69682b6683
Angelo Ricardo <angeloengcomp@gmail.com>
2021-04-16 02:38:05 -0300

    fragments

---
+++ fundamentos/src/components/basics/Fragmento.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Fragmento (props){
+    return(
+
+        
+        // <React.Fragment>
+        //     <h1>Other Form</h1>
+        //     <p>Can you use this form, case you want use properties how "React.Fragments key="etctctc"</p>
+
+        // </React.Fragment>
+        
+        <>
+        <h1>Fragment</h1>
+        <p>Forms to use Fragments. Plis, inspect the element
+        </p>
+        </>
+
+
+
+    )
+}
--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './index.css'
 import Primeiro from './components/basics/primeiro'
 import ComParametro from './components/basics/ComParametros'
-
+import Fragment from './components/basics/Fragmento'
 
 ReactDOM.render(
 
@@ -14,6 +14,7 @@ ReactDOM.render(
         aluno='Angelo Ricardo'
         nota={8.5}
         />
+        <Fragment/>
         
     </div>
 

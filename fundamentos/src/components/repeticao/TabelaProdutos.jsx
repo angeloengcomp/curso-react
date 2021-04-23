e5635d5ecbc98d2063e13b31df5bfb87b277818c
Angelo Ricardo <angeloengcomp@gmail.com>
2021-04-23 01:41:53 -0300

    Desafio Tabela

--- fundamentos/src/components/repeticao/TabelaProdutos.jsx
+++ fundamentos/src/components/repeticao/TabelaProdutos.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import tabela from '../../data/produtos';
+import produtos from '../../data/produtos';
+import './TabelaProdutos.css'
 
+export default (props) => {
 
-export default (props) =>{
-    const tabelaProdutos = tabela.map((tabela)=>{
-        return(
-            <tr key={tabela.id}>
-                <td>{tabela.id}</td><td>{tabela.nome}</td><td>{tabela.preco}</td>
-            </tr>
-        )
-    });
+    function getLinhas(){
+        return produtos.map((produto,i) => {
+            return (
+                <tr key={produtos.id} className={i % 2==0? 'Par':'Impar'}>    
 
-    return(
-        
-        <div>
+                <td>{produto.id}</td> <td>{produto.nome}</td> <td>R$ {produto.preco.toFixed(2).replace('.',',')}</td>    
 
-            <table>
-                {tabelaProdutos}
+                </tr>
+            )            
+        })
+    }
+    
+    return (
+
+        <div className="tabelaProdutos">
+
+            <table border="0" >
+                <thead>
+                    <tr>
+                        <th>ID</th> <th>Produto</th>   <th>Preço</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {/*Função de  Repetição */}
+                    {getLinhas()}
+                </tbody>
             </table>
 
         </div>
--- fundamentos/src/data/produtos.js
+++ fundamentos/src/data/produtos.js
@@ -1,9 +1,13 @@
-export default [
-
-    {id: 1, nome: 'Celular', preco: 999.99},
-    {id: 2, nome: 'Smart Watch', preco: 399.99},
-    {id: 3, nome: 'Smart Tv', preco: 1050.99},
-    {id: 4, nome: 'Headset', preco: 250.99},
-
+const produtos = [
+    
+    {id: 1, nome: 'Celular', preco: 999.923239},
+    {id: 2, nome: 'Smart Watch', preco: 399.923239},
+    {id: 3, nome: 'Smart Tv', preco: 1050.991212},
+    {id: 4, nome: 'Headset', preco: 250.1121299},
+    {id: 5, nome: 'Sapato', preco: 50.92323239},
+    {id: 6, nome: 'Camiseta', preco: 450.99121212},
+    {id: 7, nome: 'Chapeu', preco: 350.923239},
 
 ]
+
+export default produtos

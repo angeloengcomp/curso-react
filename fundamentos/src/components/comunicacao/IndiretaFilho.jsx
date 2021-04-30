f34ba10a4cfcc7934d85e841e16a49be34633e2a
Angelo Ricardo <angeloengcomp@gmail.com>
2021-04-30 17:37:42 -0300

    indireta filho

--- fundamentos/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default props => {
-
-
+    const cb=props.qndClicar
     return (
         <div>
-
+            <div>
+                Filho
+            </div>
+            <button onClick={_=>cb('joão', 34, true)}> ENVIAR INFORMAÇÕES PARA O PAI</button>
         </div>
     )
 }
--- fundamentos/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos/src/components/comunicacao/IndiretaPai.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import IndiretaFilho from './IndiretaFilho'
 
-export default props =>{
-
-    return(
+export default props => {
+    let nome = '?'
+    let idade = 0
+    let nerd = false
+    function fornecerInformacoes(nomeParam, idadeParam, nerdParam) {
+        nome = nomeParam
+        idade = idadeParam
+        nerd = nerdParam
+    }
+    return (
         <div>
-            Filho
+            <div>
+                <span>{nome} </span>
+                <span>{idade} </span>
+                <span>{nerd ? 'é Nerd' : 'Não é Nerd'}</span>
+            </div>
+            <IndiretaFilho qndClicar={fornecerInformacoes}></IndiretaFilho>
         </div>
     )
 }
